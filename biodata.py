521e6317e03b0701c7f26d86c2a153a718d985cc
Kiyaa13 <139347887+Kiyaa13@users.noreply.github.com>
2024-07-27 11:55:13 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,9 @@
+nama = "dzakiyah talita S"
+kelas = "XI IPA 9"
+menurutku = "coding sangat susah "
+
+print("nama saya adalah :", nama)
+print("kelas saya adalah :", kelas)
+print("menurut saya", menurutku)
+
+
